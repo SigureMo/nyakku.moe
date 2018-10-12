7c85f339fdd964808ec6c8296b64b58d51a52af7
SigureMo <sigure_mo@163.com>
2018-10-12 12:54:01 +0800

    :bulb: Add Ocr_du1.2

--- Codes/Python/aip/OCR_du/OCR_du.py
+++ Codes/Python/aip/OCR_du/OCR_du.py
@@ -1,4 +1,6 @@
 import requests,base64,urllib,json,time,os,pickle,xlwt,re
+from PIL import Image
+from PIL import ImageEnhance
 
 #时间处理模块：
 def time_now():
@@ -65,7 +67,7 @@ def json2xls(json,name):
             sheetname='Temp'
     sheet=wb.add_sheet(sheetname)
     num_qian=re.compile(r'-?(\d{1,3},)?(\d{3},)*(\d{3})(.(\d+))?')
-    num=re.compile(r'\d+(.(\d+))?$')
+    num=re.compile(r'-?\d+(.(\d+))?$')
     try:
         for body in bodys:
             if num_qian.match(body.get('word')):
@@ -78,34 +80,41 @@ def json2xls(json,name):
                 sheet.write(body.get('row')[0],body.get('column')[0],body.get('word'))
         flag=True
     except:
-        with open(name+'.dat','wb') as f:
+        with open(temp+os.sep+name+'.dat','wb') as f:
             pickle.dump(json,f)
         flag=False
     finally:
-        wb.save(name+'.xls')
+        wb.save(xlsOutput+os.sep+name+'.xls')
         return flag
 
 def find_image():
     names=[]
     images=['jpg','bmp','jpeg','png']
-    for file in os.listdir('.'):
+    for file in os.listdir(imageInput):
         if file.split('.')[-1] in images:
             names.append(('.'.join(file.split('.')[:-1]),\
                           '.'+file.split('.')[-1]))
     return names
 
 
-def to_excel():
+def to_excel(enhance):
     names=find_image()
     if names:
         for file in names:
             name=file[0]
             exname=file[1]
-            if not os.path.exists(name+'.xls'):
-                print('已发现',name+exname,'准备提交……')
-                json=up_to_baidu(name+exname)
+            if not os.path.exists(xlsOutput+os.sep+name+'.xls'):
+                if enhance:
+                    print('已发现',name+exname,'正在增强处理……')
+                    if image_enhance(file):
+                        json=up_to_baidu(temp+os.sep+name+'.jpg')
+                    else:
+                        json=up_to_baidu(imageInput+os.sep+name+exname)
+                else:
+                    print('已发现',name+exname,'准备提交……')
+                    json=up_to_baidu(imageInput+os.sep+name+exname)
                 if json2xls(eval(json),name):
-                    print('已成功保存'+name+'.xls，请在本文件夹内寻找')
+                    print('已成功保存'+name+'.xls，请在{}内寻找'.format(xlsOutput))
                 else:
                     print('保存'+name+'.xls时发生未知错误，请联系开发人员')
             else:
@@ -114,21 +123,60 @@ def to_excel():
         print('未检测到图片文件（jpg、jpeg、png、bmp）')
 
 def check(password):#密码校验
-    pd='****'
+    pd='0227'
     while password!=pd:
         password=input('密码错误，请重新输入密码：')
         if password==pd:
             break
     print('欢迎使用OCR_du！')
 
+def image_enhance(file):
+    try:
+        im=Image.open(imageInput+os.sep+file[0]+file[1])
+        if im.size[0]<15 or im.size[1]<15 or \
+           im.size[0]>4096 or im.size[1]>4096:
+            print('图片大小不符，正在处理……')
+            im.thumbnail((4000,int(im.size[1]/im.size[0]*4000)))
+        om=ImageEnhance.Contrast(im)
+        om.enhance(10).save(temp+os.sep+file[0]+'.jpg')
+        print('成功对图片进行增强处理，准备提交……')
+        return True
+    except:
+        print('处理失败，将提交原图片')
+        return False
+
 def main():
     '''if not os.path.exists('log.txt'):
         loginit()'''
+    if not os.path.exists(temp):
+        os.mkdir(temp)
+    if not os.path.exists(imageInput):
+        os.mkdir(imageInput)
+    if not os.path.exists(xlsOutput):
+        os.mkdir(xlsOutput)
     password=input('请输入密码：')
+    k=input('是否对图像进行增强处理？[y/n]')
+    if k and k[0] in 'Nn':
+        enhance=False
+    else:
+        enhance=True
     check(password)
-    to_excel()
+    to_excel(enhance)
     input('Press <Enter>')
+
+temp='Temp'
+imageInput='Input'
+xlsOutput='Output'
+
 
 if __name__=='__main__':
     main()
+
+'''def test():
+    if not os.path.exists(temp):
+        os.mkdir(temp)
+
+test()'''
+
+
 
