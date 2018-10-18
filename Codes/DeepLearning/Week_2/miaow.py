c3a7b8331dbb6baa1bf2a3977f11d88ced14b7d6
SigureMo <sigure_mo@163.com>
2018-10-18 19:42:11 +0800

    :bulb: Add DL Week2

---
+++ Codes/DeepLearning/Week_2/miaow.py
@@ -0,0 +1,165 @@
+'''
+暂时还不太了解如何构建，copy的
+原文链接：https://blog.csdn.net/u013733326/article/details/79639509
+数据集依赖：https://pan.baidu.com/s/1Uui641jlRL9JD9WS-z3yRA
+太多无用的注释严重影响阅读性，故注释掉
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+import h5py
+from lr_utils import load_dataset
+
+def sigmoid(z):
+    """
+    计算sigmod
+    """
+    s = 1 / (1 + np.exp(-z))
+    return s
+
+def initialize_with_zeros(dim):
+    """
+    初始化w和b
+    """
+    w = np.zeros(shape = (dim,1))
+    b = 0
+    #使用断言来确保我要的数据是正确的
+    assert(w.shape == (dim, 1)) #w的维度是(dim,1)
+    assert(isinstance(b, float) or isinstance(b, int)) #b的类型是float或者是int
+
+    return (w , b)
+
+def propagate(w, b, X, Y):
+    """
+    实现前向和后向传播的成本函数及其梯度。
+    """
+    m = X.shape[1]
+
+    #正向传播
+    A = sigmoid(np.dot(w.T,X) + b) #计算激活值，请参考公式2。
+    cost = (- 1 / m) * np.sum(Y * np.log(A) + (1 - Y) * (np.log(1 - A))) #计算成本，请参考公式3和4。
+
+    #反向传播
+    dw = (1 / m) * np.dot(X, (A - Y).T) #请参考视频中的偏导公式。
+    db = (1 / m) * np.sum(A - Y) #请参考视频中的偏导公式。
+
+    #使用断言确保我的数据是正确的
+    assert(dw.shape == w.shape)
+    assert(db.dtype == float)
+    cost = np.squeeze(cost)
+    assert(cost.shape == ())
+
+    #创建一个字典，把dw和db保存起来。
+    grads = {
+                "dw": dw,
+                "db": db
+             }
+    return (grads , cost)
+
+def optimize(w , b , X , Y , num_iterations , learning_rate , print_cost = False):
+    """
+    此函数通过运行梯度下降算法来优化w和b
+    """
+
+    costs = []
+
+    for i in range(num_iterations):
+
+        grads, cost = propagate(w, b, X, Y)
+
+        dw = grads["dw"]
+        db = grads["db"]
+
+        w = w - learning_rate * dw
+        b = b - learning_rate * db
+
+        #记录成本
+        if i % 100 == 0:
+            costs.append(cost)
+        #打印成本数据
+        if (print_cost) and (i % 100 == 0):
+            print("迭代的次数: %i ， 误差值： %f" % (i,cost))
+
+    params  = {
+                "w" : w,
+                "b" : b }
+    grads = {
+            "dw": dw,
+            "db": db } 
+    return (params , grads , costs)
+
+def predict(w , b , X ):
+    """
+    使用学习逻辑回归参数logistic （w，b）预测标签是0还是1，
+    """
+
+    m  = X.shape[1] #图片的数量
+    Y_prediction = np.zeros((1,m)) 
+    w = w.reshape(X.shape[0],1)
+
+    #计预测猫在图片中出现的概率
+    A = sigmoid(np.dot(w.T , X) + b)
+    for i in range(A.shape[1]):
+        #将概率a [0，i]转换为实际预测p [0，i]
+        Y_prediction[0,i] = 1 if A[0,i] > 0.5 else 0
+    #使用断言
+    assert(Y_prediction.shape == (1,m))
+
+    return Y_prediction
+
+def model(X_train , Y_train , X_test , Y_test , num_iterations = 2000 , learning_rate = 0.5 , print_cost = False):
+    """
+    通过调用之前实现的函数来构建逻辑回归模型
+    """
+    w , b = initialize_with_zeros(X_train.shape[0])
+
+    parameters , grads , costs = optimize(w , b , X_train , Y_train,num_iterations , learning_rate , print_cost)
+
+    #从字典“参数”中检索参数w和b
+    w , b = parameters["w"] , parameters["b"]
+
+    #预测测试/训练集的例子
+    Y_prediction_test = predict(w , b, X_test)
+    Y_prediction_train = predict(w , b, X_train)
+
+    #打印训练后的准确性
+    print("训练集准确性："  , format(100 - np.mean(np.abs(Y_prediction_train - Y_train)) * 100) ,"%")
+    print("测试集准确性："  , format(100 - np.mean(np.abs(Y_prediction_test - Y_test)) * 100) ,"%")
+
+    d = {
+            "costs" : costs,
+            "Y_prediction_test" : Y_prediction_test,
+            "Y_prediciton_train" : Y_prediction_train,
+            "w" : w,
+            "b" : b,
+            "learning_rate" : learning_rate,
+            "num_iterations" : num_iterations }
+    return d
+
+train_set_x_orig , train_set_y , test_set_x_orig , test_set_y , classes = load_dataset()
+index = 25
+# plt.imshow(train_set_x_orig[index])
+# plt.show()
+m_train = train_set_y.shape[1] #训练集里图片的数量。
+m_test = test_set_y.shape[1] #测试集里图片的数量。
+num_px = train_set_x_orig.shape[1] #训练、测试集里面的图片的宽度和高度（均为64x64）。
+
+# 降维
+#X_flatten = X.reshape(X.shape [0]，-1).T ＃X.T是X的转置
+train_set_x_flatten  = train_set_x_orig.reshape(train_set_x_orig.shape[0],-1).T
+test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1).T
+
+# 标准化数据集
+train_set_x = train_set_x_flatten / 255
+test_set_x = test_set_x_flatten / 255
+
+# 训练模型，并进行测试
+d = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations = 2000, learning_rate = 0.005, print_cost = True)
+
+#绘制图
+costs = np.squeeze(d['costs'])
+plt.plot(costs)
+plt.ylabel('cost')
+plt.xlabel('iterations (per hundreds)')
+plt.title("Learning rate =" + str(d["learning_rate"]))
+plt.show()
