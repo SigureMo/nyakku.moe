e14b9c15f57c1595b1707554414fa46093720dc1
SigureMo <sigure_mo@163.com>
2018-10-18 19:42:11 +0800

    :bulb: Add DL Week2

--- Codes/Python/os/OTI.py
+++ Codes/Python/os/OTI.py
@@ -4,6 +4,12 @@ libs={'requests','pillow','xlwt','xlrd','BeautifulSoup4','pyinstaller',\
       'wheel','networkx','sympuy','django','flask','werobot','pyqt5',\
       'pandas','pyopengl','pypdf2','docopt','pygame'}
 
+libs = {
+    'matplotlib',
+    'h5py',
+    'numpy',
+    }
+
 for lib in libs:
     try:
         os.system('pip install '+lib+' -i https://pypi.tuna.tsinghua.edu.cn/simple')
