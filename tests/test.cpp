fe6c75793f6209912cab9c69d67693e4451cb9b9
SigureMo <sigure_mo@163.com>
2019-01-04 23:45:55 +0800

    :memo: Add SPT08
    完成固态相变第八章~

---
+++ tests/test.cpp
@@ -0,0 +1,17 @@
+// #include <iostream> 
+// #include <cmath>
+#include <stdio.h>
+#include <math.h>
+
+// using namespace std;
+
+int main(){
+	int i = 0;
+	while (1){
+		printf("\r%d", i);
+		//cout << '\r' <<i;
+		i++;
+	}
+	
+	return 0;
+}
