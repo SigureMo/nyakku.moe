c2c27a0018e8f35844c27f99287ddae9bb0ec2b8
SigureMo <sigure_mo@163.com>
2019-02-05 19:52:19 +0800

    :bug: Fix bug for bilili-dl
    - 修复 timer 线程提前退出的问题
    - 新的一年，bug ……退散？

--- Codes/pynotex/utils/thread.py
+++ Codes/pynotex/utils/thread.py
@@ -1,7 +1,11 @@
 import queue
 import threading
 
+
 class ThreadPool():
+    """ 线程池类
+    快速创建多个相同任务的线程池
+    """
     def __init__(self, num):
         self.num = num
         self._taskQ = queue.Queue()
@@ -10,7 +14,7 @@ class ThreadPool():
     def add_task(self, task):
         self._taskQ.put(task)
 
-    def run_task(self):
+    def _run_task(self):
         while True:
             if not self._taskQ.empty():
                 task = self._taskQ.get(block = True, timeout = 1)
@@ -21,7 +25,7 @@ class ThreadPool():
 
     def run(self):
         for i in range(self.num):
-            th = threading.Thread(target=self.run_task)
+            th = threading.Thread(target=self._run_task)
             th.setDaemon(True)
             self.threads.append(th)
             th.start()
