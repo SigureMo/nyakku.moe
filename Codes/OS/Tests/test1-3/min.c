5d0b1484388dac382d228ed45ec59863306cfcc9
SigureMo <sigure_mo@163.com>
2019-04-27 00:06:59 +0800

    :sparkles: feat(OS): update OS test
    完成 OS 实验 1-3，也就是完成实验 1 全部内容

--- Codes/OS/Tests/test1-3/min.c
+++ Codes/OS/Tests/test1-3/min.c
@@ -3,13 +3,13 @@
 
 int main(int argc, char* argv[]) {
 	int i;
-	double max = atof(argv[1]);
+	double min = atof(argv[1]);
 	for (i = 2; i < argc; i++) {
 		double t = atof(argv[i]);
-		if (t > max) {
-			max = t;
+		if (t < min) {
+			min = t;
 		}
 	}
-	printf("Max num is %g\n", max);
+	printf("Min num is %g\n", min);
 	return 0;
 }
