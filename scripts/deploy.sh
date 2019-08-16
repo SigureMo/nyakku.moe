45c394bfe49525534bdefef494ff87b518fb2740
SigureMo <sigure_mo@163.com>
2019-08-16 17:43:05 +0800

    :sparkles: docs(Maxout): update Maxout.md
    修复 Maxout 一文中的错误描述，并使用新的描述方式
    修复 文档安全问题

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -16,7 +16,7 @@ git init
 git add -A
 # git commit -m 'deploy'
 time=$(date "+%Y-%m-%d %H:%M:%S")
-git commit -m "redeploy @${time}"
+git commit -m "rebuild @${time}"
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
