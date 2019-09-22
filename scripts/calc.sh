6ab6328bf5aa3b1f430accb20c7ecfa4384b586f
SigureMo <sigure_mo@163.com>
2019-09-23 00:40:37 +0800

    :sparkles: feat(calc): optmize calc
    优化 计算器
    开始 写 git-bin 以取代 U 盘传输同步二进制文件的方式

--- scripts/calc.sh
+++ scripts/calc.sh
@@ -1,8 +1,8 @@
 g++ -std=c++11 -c Codes/VB/05_calc/calculate/arithmetic_unit.cpp -o tmp/arithmetic_unit.o
-g++ -std=c++11 -c Codes/VB/05_calc/calculate/calculate.cpp -o tmp/calculate.o
+g++ -std=c++11 -c Codes/VB/05_calc/calculate/calculator.cpp -o tmp/calculator.o
 g++ -std=c++11 -c Codes/VB/05_calc/calculate/operand.cpp -o tmp/operand.o
 g++ -std=c++11 -c Codes/VB/05_calc/calculate/operator.cpp -o tmp/operator.o
 g++ -std=c++11 -c Codes/VB/05_calc/calculate/compute.cpp -o tmp/compute.o
-g++ -std=c++11 tmp/calculate.o tmp/arithmetic_unit.o tmp/operand.o tmp/operator.o tmp/compute.o -o tmp/calculator
+g++ -std=c++11 tmp/calculator.o tmp/arithmetic_unit.o tmp/operand.o tmp/operator.o tmp/compute.o -o tmp/calculator
+rm tmp/arithmetic_unit.o tmp/calculator.o tmp/operand.o tmp/operator.o tmp/compute.o
 tmp/calculator
-yarn clear
