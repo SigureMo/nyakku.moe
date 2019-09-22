48171cb116209ea492bbbae2b5038fa982763894
SigureMo <sigure_mo@163.com>
2019-09-22 19:56:42 +0800

    :fire: style(scripts): clean codes
    整理 脚本代码
    增加 快速运行代码的脚本，run.py，快速启动方式 yarn r <file>

--- Codes/Data_Structures_and_Algorthms/Chapter_03_Stack/04_Sequence_Stack_oop.cpp
+++ Codes/Data_Structures_and_Algorthms/Chapter_03_Stack/04_Sequence_Stack_oop.cpp
@@ -2,37 +2,38 @@
 #define MaxSize 50
 typedef int ElemType;
 
-class Stack{
-	public:
-		Stack(){
-			top = -1;
-		}
-		bool isEmpty(){
-			return (top == -1);
-		}
-		ElemType peek(){
-			return data[top];
-		}
-		ElemType push(ElemType value){
-			return data[++top] = value;
-		}
-		ElemType pop(){
-			return data[top--];
-		}
-		int getSize(){
-			return top+1;
-		}
-	private:
-		int data[MaxSize];
-		int top;
+class Stack {
+public:
+  Stack() {
+    top = -1;
+  }
+  bool isEmpty() {
+    return (top == -1);
+  }
+  ElemType peek() {
+    return data[top];
+  }
+  ElemType push(ElemType value) {
+    return data[++top] = value;
+  }
+  ElemType pop() {
+    return data[top--];
+  }
+  int getSize() {
+    return top+1;
+  }
+private:
+  int data[MaxSize];
+  int top;
 };
 
-int main(){
-	Stack stack;
-	for (int i = 0; i < 10; i++){
-		stack.push(i);
-	}
-	while(!stack.isEmpty())
-	  std::cout << stack.pop() << std::endl;
-	return 0;
+int main() {
+  Stack stack;
+  for (int i = 0; i < 10; i++) {
+    stack.push(i);
+  }
+  while (!stack.isEmpty()) {
+    std::cout << stack.pop() << std::endl;
+  }
+  return 0;
 }
