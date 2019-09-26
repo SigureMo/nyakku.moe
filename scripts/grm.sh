092bc9b1b84a236469618853ee6dd88a66c99fa8
SigureMo <sigure_mo@163.com>
2019-09-26 20:46:40 +0800

    :sparkles: feat(scripts): add grm.sh
    新增 grm.sh 及其快速调用脚本，方便快速彻底删除 git 中的文件

---
+++ scripts/grm.sh
@@ -0,0 +1,8 @@
+git filter-branch --force --index-filter \
+‘git rm --cached --ignore-unmatch $1’ \
+--prune-empty --tag-name-filter cat -- --all
+rm -rf .git/refs/original/
+git reflog expire --expire=now --all
+git gc --prune=now
+git gc --aggressive --prune=now
+git push origin master --force
