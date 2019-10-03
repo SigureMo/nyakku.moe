9e59da969673ec52da4e918237e9076d84e6302e
SigureMo <sigure.qaq@gmail.com>
2019-10-04 03:53:47 +0800

    :zap: style(pynotex): improve format of pynotex
    优化 pynotex 中 data_root 和 tmp_root 声明方式

--- Codes/pynotex/common/app.py
+++ Codes/pynotex/common/app.py
@@ -3,11 +3,12 @@ import os
 from common.config import Config
 from common.file import Dir
 
-DATA_ROOT = "data/"
-TMP_ROOT = "tmp/"
+CONFIG = Config()
+DATA_ROOT = CONFIG["data_root"]
+TMP_ROOT = CONFIG["tmp_root"]
 
 class App():
     def __init__(self, name):
-        self.config = Config(name)
+        self.config = CONFIG.switch_app(name)
         self.data_dir = Dir(os.path.join(DATA_ROOT, name))
         self.tmp_dir = Dir(os.path.join(TMP_ROOT, name))
