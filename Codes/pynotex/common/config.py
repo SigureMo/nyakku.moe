7b8af5c8723e2bf17eccbad1a4ad6a0e07fe4f32
SigureMo <sigure_mo@163.com>
2019-10-04 03:53:47 +0800

    :zap: style(pynotex): improve format of pynotex
    优化 pynotex 中 data_root 和 tmp_root 声明方式

--- Codes/pynotex/common/config.py
+++ Codes/pynotex/common/config.py
@@ -5,19 +5,24 @@ CONF_PATH = 'Codes/pynotex/config.json'
 ''' config.json
 {
   "name": "pynotex",
+  "data_root": "data/",
+  "tmp_root": "tmp/",
   "APPs": {
-    "wenku": {
-      "tmp_dir": "tmp/wenku/",
-      "data_dir": "data/wenku/"
-    },
-    "check_time": {},
+    "wenku": {},
+    "markdown-mail": {
+      "footer": "footer.md",
+      "username": "SigureMo"
+    }
   }
 }
 // 根据所使用的 APP 添加配置项
 '''
 
 class Config(dict):
-    def __init__(self, name, config_path=CONF_PATH):
+    def __init__(self, config_path=CONF_PATH):
         with open(config_path, 'r') as f:
             _jObject = json.load(f)
-        super().__init__(_jObject['APPs'].get(name, {}))
+        super().__init__(_jObject)
+
+    def switch_app(self, name):
+        return self['APPs'].get(name, {})
