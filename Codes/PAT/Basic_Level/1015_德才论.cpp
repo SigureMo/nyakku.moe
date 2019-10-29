cdecde9f946da4d43b4ed61694d2b6c16ed1317f
SigureMo <sigure_mo@163.com>
2019-10-29 23:59:31 +0800

    :zap: refactor(PAT): improve B101{2,5}
    优化 乙级 1012 代码
    完成 乙级 1015 未完成两个测试点
    移除 乙级内的 README 文件
    优化 docs-dev.py ，增加自动移除 theme 字段
    精简 README 文件

--- Codes/PAT/Basic_Level/1015_德才论.cpp
+++ Codes/PAT/Basic_Level/1015_德才论.cpp
@@ -1,76 +1,67 @@
-#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
-// 为什么直接插入排序会比先 push_back 后 vector 慢？还没弄明白，速度慢了不是一点点，这样的算法甚至过不了 4、5
+using namespace std;
 
-struct student {
+typedef struct student {
   int number;
   int de;
   int cai;
-};
+} Student;
 
-typedef struct student Student;
-Student team1[100000];
-Student team2[100000];
-Student team3[100000];
-Student team4[100000];
-
-void print(Student team[], int cnt);
-int cmp(Student* stu1, Student* stu2);
-void insert(Student team[], Student stu, int cnt);
+bool cmp(Student stu1, Student stu2);
+void print(vector<Student> team);
 
 int main() {
   int N, L, H;
-  int cnt1 = 0, cnt2 = 0, cnt3 = 0, cnt4 = 0;
+  int cnt[4] = {0};
   Student stu;
+  vector<Student> teams[4];
   scanf("%d %d %d", &N, &L, &H);
   while (N--) {
     scanf("%d %d %d", &stu.number, &stu.de, &stu.cai);
     if (stu.de < L || stu.cai < L) {}
     else if (stu.de >= H && stu.cai >= H) {
-      cnt1++;
-      insert(team1, stu, cnt1);
+      cnt[0]++;
+      teams[0].push_back(stu);
     }
     else if (stu.de >= H) {
-      cnt2++;
-      insert(team2, stu, cnt2);
+      cnt[1]++;
+      teams[1].push_back(stu);
     }
     else if (stu.de >= stu.cai) {
-      cnt3++;
-      insert(team3, stu, cnt3);
+      cnt[2]++;
+      teams[2].push_back(stu);
     }
     else {
-      cnt4++;
-      insert(team4, stu, cnt4);
+      cnt[3]++;
+      teams[3].push_back(stu);
     }
   }
-  printf("%d\n", cnt1 + cnt2 + cnt3 + cnt4);
-  print(team1, cnt1);
-  print(team2, cnt2);
-  print(team3, cnt3);
-  print(team4, cnt4);
+  for (auto &team : teams) {
+    sort(team.begin(), team.end(), cmp);
+  }
+  printf("%d\n", cnt[0] + cnt[1] + cnt[2] + cnt[3]);
+  for (auto team : teams) {
+    print(team);
+  }
   return 0;
 }
 
-void print(Student team[], int cnt) {
-  for (int i = 0; i < cnt; i++) {
-    printf("%d %d %d\n", team[i].number, team[i].de, team[i].cai);
+void print(vector<Student> team) {
+  for (auto stu : team) {
+    printf("%d %d %d\n", stu.number, stu.de, stu.cai);
   }
 }
 
-int cmp(Student* stu1, Student* stu2) {
-  if ((stu1->de + stu1->cai) != (stu2->de + stu2->cai)) {
-    return (stu1->de + stu1->cai) > (stu2->de + stu2->cai);
+bool cmp(Student stu1, Student stu2) {
+  if ((stu1.de + stu1.cai) != (stu2.de + stu2.cai)) {
+    return (stu1.de + stu1.cai) > (stu2.de + stu2.cai);
   }
-  else if (stu1->de != stu2->de) {
-    return stu1->de > stu2->de;
+  else if (stu1.de != stu2.de) {
+    return stu1.de > stu2.de;
   }
   else
-    return stu1->number < stu2->number;
-}
-
-void insert(Student team[], Student stu, int cnt) {
-  for (; cnt > 0 && cmp(&stu, &team[cnt-1]); cnt--) {
-    team[cnt] = team[cnt-1];
-  }
-  team[cnt] = stu;
+    return stu1.number < stu2.number;
 }
