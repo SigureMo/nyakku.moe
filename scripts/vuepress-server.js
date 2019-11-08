979d7b45ad45e4b7b12ec56ca7beae002ee61c7a
SigureMo <sigure_mo@163.com>
2019-11-08 23:05:32 +0800

    :sparkles: feat(scripts): add vuepress-server.js
    新增 本地 VuePress 静态服务器启动脚本，以便快速 build 后启动服务器测试

---
+++ scripts/vuepress-server.js
@@ -0,0 +1,45 @@
+// based on
+// https://blog.csdn.net/github_39457740/article/details/88780914
+
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
+const path = require("path");
+const docs = process.argv[2];
+const docsDir = path.resolve(process.cwd(), docs);
+const config = require(`${docsDir}/.vuepress/config.js`);
+const { dest = ".vuepress/dist" } = config;
+const root = path.join(docsDir, dest);
+const port = 8090;
+
+let server = http.createServer(function(request, response) {
+  let pathname = url.parse(request.url, true).pathname;
+  filePath = path.join(root, pathname);
+  if (pathname == "/") {
+    filePath = path.join(filePath, "/index.html");
+  }
+
+  fs.readFile(filePath, function(err, data) {
+    if (err) {
+      console.log(err);
+      let errPath = path.join(root, "/404.html");
+      fs.readFile(errPath, (err, data404) => {
+        if (err) {
+          response.write("404 Not Found");
+          response.end();
+        } else {
+          response.writeHead(404, {
+            "Content-Type": "text/html;charset='utf-8'"
+          });
+          response.write(data404);
+          response.end();
+        }
+      });
+    } else {
+      response.write(data);
+      response.end();
+    }
+  });
+});
+server.listen(port);
+console.log(`> VuePress test server listening at http://localhost:${port}/`);
