cad596902e6ac544c30994c1257e505f1facce01
SigureMo <sigure_mo@163.com>
2019-11-09 18:49:31 +0800

    :construction_worker: docs(_): use github action
    尝试 新的 CI ，使用 GitHub action
    
    :green_heart: try to fix CI
    
    :art: docs(_): update theme to 1.1.2-alpha.2
    
    :green_hrear: try to use yarn instead of npm
    
    :green_hreat: try to use yarn instead of npm

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,26 @@
 #!/usr/bin/env sh
 
+ACCESS_TOKEN=$1
+USERNAME=SigureMo       # 你的用户名
+REPO=""                 # 如果不指定，将发布在 <username>.github.io
+BRANCH=""               # 如果不指定，将发布在 master 分支
+CNAME=www.sigure.xyz    # 你想发布到的域名
+
+if [ $ACCESS_TOKEN ]
+then TOKEN_PREFIX="${ACCESS_TOKEN}@"
+else TOKEN_PREFIX=""
+fi
+
+if [ $BRANCH ]
+then BRANCH_POSTFIX=":${BRANCH}"
+else BRANCH_POSTFIX=""
+fi
+
+if [ $REPO ]
+then REMOTE=https://${TOKEN_PREFIX}github.com/${USERNAME}/${REPO}.git
+else REMOTE=https://${TOKEN_PREFIX}github.com/${USERNAME}/${USERNAME}.github.io.git
+fi
+
 # 确保脚本抛出遇到的错误
 set -e
 
@@ -10,18 +31,17 @@ npm run docs:build
 cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名
-# echo 'www.sigure.xyz' > CNAME
+if [ $CNAME ]
+then echo $CNAME > CNAME
+fi
 
+# 初始化仓库并提交发布
 git init
+git config user.name "GitHub Actions"
+git config user.email "support@github.com"
 git add -A
-# git commit -m 'deploy'
 time=$(date "+%Y-%m-%d %H:%M:%S")
 git commit -m "rebuild @${time}"
-
-# 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:SigureMo/SigureMo.github.io.git master
-
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:SigureMo/notev.git master:gh-pages
+git push -f $REMOTE master${BRANCH_POSTFIX}
 
 cd -
