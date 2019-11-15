2c8eb218cb308bab6b190c805ac6ec06172aec77
SigureMo <sigure_mo@163.com>
2019-11-16 01:28:01 +0800

    :review: refactor(DS2.0): review Insertion Sort
    复习 插入排序内容
    优化 Kannada-MNIST，使用 Kaggle 在线笔记本
    优化 CN2.0 中 ARQ 部分

--- Codes/Data_Structures_and_Algorthms/Chapter_09_Sort/01_Insertion_Sort.cpp
+++ Codes/Data_Structures_and_Algorthms/Chapter_09_Sort/01_Insertion_Sort.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-// :::
+// SIMPLE_INSERT
 template<typename T>
 void Insertion_Sort(T a[], int N){
 	int i;
@@ -14,7 +14,60 @@ void Insertion_Sort(T a[], int N){
 		a[i] = tmp;
 	}
 }
-// :::
+// SIMPLE_INSERT
+// HALF_INSERT
+template<typename T>
+void Half_Insertion_Sort(T a[], int N) {
+	int i, low, high, mid;
+	for (int P = 1; P < N; P++) {
+		T tmp = a[P];
+    low = 0;
+    high = P;
+    while (low <= high) {
+      mid = (low + high) / 2;
+      if (tmp < a[mid]) high = mid - 1;
+      else low = mid + 1;
+    }
+		for (i = P; i > low; i--) {
+			a[i] = a[i-1];
+		}
+		a[i] = tmp;
+	}
+}
+// HALF_INSERT
+// SHELL_INSERT
+template<typename T>
+void Shell_Sort(T a[], int N) {
+	int i;
+	for (int D = N/2; D > 0; D /= 2) {
+		for (int P = D; P < N; P++){
+		    T tmp = a[P];
+		    for (i = P; i >= D && a[i-D] > tmp; i-=D) {
+			    a[i] = a[i-D];
+		    }
+		    a[i] = tmp;
+	    }
+	}
+}
+// SHELL_INSERT
+// BUBBLE_SWAP
+template<typename T>
+void Bubble_Sort(T a[], int N) {
+	for (int P = N-1; P >= 0; P--) {
+		bool flag = false;
+		for (int i = 0; i < P; i++) {
+			if (a[i] > a[i+1]) {
+				T t = a[i];
+				a[i] = a[i+1];
+				a[i+1] = t;
+				flag = true;
+		  }
+		}
+		if (!flag)
+			break;
+	}
+}
+// BUBBLE_SWAP
 
 template<typename T>
 bool check(T a[], T b[], int N) {
@@ -24,11 +77,35 @@ bool check(T a[], T b[], int N) {
   return true;
 }
 
-int main() {
+template<typename T>
+void print(T a[], int N) {
+  for (int i = 0; i < N; i++) {
+    cout << a[i] << " ";
+  }
+  cout << endl;
+}
+
+int main(int argc, char* argv[]) {
+  string mode = argv[1];
   int a[] = {4, 981, 10, -17, 0, -20, 29, 50, 8, 43, -5};
   int b[] = {-20, -17, -5, 0, 4, 8, 10, 29, 43, 50, 981};
   int N = sizeof(a)/sizeof(a[0]);
-  Insertion_Sort(a, N);
+  if (mode == "insertion") {
+    Insertion_Sort(a, N);
+  }
+  else if (mode == "half_insertion") {
+    Half_Insertion_Sort(a, N);
+  }
+  else if (mode == "shell") {
+    Shell_Sort(a, N);
+  }
+  else if (mode == "bubble") {
+    Bubble_Sort(a, N);
+  }
+  else {
+    cout << "Invaild mode!";
+    return 1;
+  }
   cout << boolalpha << "check " << (check(a, b, N) ? "passed" : "failed") << endl;
   return 0;
 }
