97435816b69d4e205c802a555eaa081536473924
SigureMo <sigure.qaq@gmail.com>
2020-05-02 20:22:23 +0800

    :sparkles: feat: add meting
    添加 插件 Meting
    修改 about 页面

--- docs/.vuepress/plugins/vuepress-plugin-meting/bin/enhanceAppFile.js
+++
@@ -1,9 +0,0 @@
-import Meting from "./Meting.vue";
-
-export default ({ Vue }) => {
-  if (typeof window !== "undefined") {
-    localStorage.setItem("aplayer-setting", "[]");
-    Vue.use(require("@moefe/vue-aplayer").default);
-  }
-  Vue.component("Meting", Meting);
-};
