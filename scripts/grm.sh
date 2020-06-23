42d5fc2794634ee7b2293e8da6a8febed2f78aa1
SigureMo <sigure.qaq@gmail.com>
2020-06-23 16:51:13 +0800

    :fire: chore: remove useless scripts

--- scripts/grm.sh
+++
@@ -1,8 +0,0 @@
-git filter-branch --force --index-filter \
-'git rm --cached --ignore-unmatch -r '$1 \
---prune-empty --tag-name-filter cat -- --all
-rm -rf .git/refs/original/
-git reflog expire --expire=now --all
-git gc --prune=now
-git gc --aggressive --prune=now
-git push origin master --force
