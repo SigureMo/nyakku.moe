902d25766cfc8c8933525266e8d725ab59c5da33
SigureMo <sigure.qaq@gmail.com>
2020-06-27 00:08:39 +0800

    :construction_worker: use actions-gh-pages instead of github-pages-deploy-action

--- scripts/upload-img.sh
+++ scripts/upload-img.sh
@@ -2,6 +2,6 @@ cd docs/img
 git pull --ff-only
 git add -A
 time=$(date "+%Y-%m-%d %H:%M:%S")
-git commit -m ":art: chore: upload images @${time}"
+git commit -m ":bento: chore: upload images @${time}"
 git push
 cd -
