a2676f5b7fd213dd70d49932bf2b5a0bb43a4d00
SigureMo <sigure.qaq@gmail.com>
2020-07-04 23:17:58 +0800

    :sparkles: feat: add friend link page

---
+++ docs/.vuepress/components/FriendLink.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="friend-link">
+    <a :href="url" :title="name" target="_blank">
+      <img :src="avatar" />
+      <p class="friend-name">{{ name }}</p>
+      <p class="friend-description">{{ description }}</p>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FriendLink",
+  props: ["name", "url", "description", "avatar"],
+};
+</script>
+
+<style scoped>
+div.friend-link {
+  width: 50%;
+  float: left;
+  position: relative;
+  padding-left: 100px;
+}
+
+div.friend-link:hover {
+  border-radius: 10px;
+  background-color: #c9f2e9;
+}
+
+img {
+  position: absolute;
+  top: 5px;
+  left: 5px;
+  width: 80px;
+  height: 80px;
+  border-radius: 50%;
+}
+
+p.friend-name {
+  padding-top: 20px;
+  font-size: 16px;
+  font-weight: bold;
+  color: black;
+  margin: 0;
+}
+
+p.friend-description {
+  padding: 15px 0;
+  margin: 0;
+  line-height: 24px;
+  color: gray;
+}
+</style>
