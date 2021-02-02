4ac6d74444486f405e9679e3d43b9fd8c908ad88
SigureMo <sigure.qaq@gmail.com>
2021-02-02 22:07:44 +0800

    📝 docs: add post javascript-prototype chain

--- src/.vuepress/components/FriendsLayout.vue
+++ src/.vuepress/components/FriendsLayout.vue
@@ -47,8 +47,8 @@ export default {
   methods: {
     shuffle(arr) {
       for (let i = arr.length - 1; i > 0; i--) {
-        const r = Math.floor(Math.random() * i)
-        ;[arr[i], arr[r]] = [arr[r], arr[i]]
+        const j = Math.floor(Math.random() * (i + 1))
+        ;[arr[i], arr[j]] = [arr[j], arr[i]]
       }
       return arr
     },
