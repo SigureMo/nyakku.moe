45f9314d4d9f5f861534fa735de22b5bb0a6fded
SigureMo <sigure.qaq@gmail.com>
2021-03-21 21:33:23 +0800

    :memo: docs: update README badges
    :arrow_up: build: bump dependencies
    :arrow_up: build: bump husky from 5.1.3 to 5.2.0 (#68)
    :arrow_up: build: bump @maginapp/vuepress-plugin-katex (#69)
    📝 docs: update README badges

--- scripts/upload-img.sh
+++ scripts/upload-img.sh
@@ -2,6 +2,6 @@ cd src/img
 git pull --ff-only
 git add -A
 time=$(date "+%Y-%m-%d %H:%M:%S")
-git commit -m ":bento: chore: upload images @${time}"
+git commit -m ":bento: chore(images): upload images @${time}"
 git push
 cd -
