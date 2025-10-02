7ce1f2b81206d1de2408e861e096b24d4775dd87
SigureMo <sigure.qaq@gmail.com>
2025-10-02 22:26:43 +0800

    ✨ feat: Add target for external link by use `rehype-external-links`

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import expressiveCode from 'astro-expressive-code'
 import icon from 'astro-icon'
 import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 import rehypeComponents from 'rehype-components' /* Render the custom directive content */
+import rehypeExternalLinks from 'rehype-external-links'
 import rehypeKatex from 'rehype-katex'
 import rehypeSlug from 'rehype-slug'
 import remarkDirective from 'remark-directive' /* Handle directives */
@@ -113,6 +114,13 @@ export default defineConfig({
     rehypePlugins: [
       rehypeKatex,
       rehypeSlug,
+      [
+        rehypeExternalLinks,
+        {
+          target: '_blank',
+          rel: ['noopener', 'noreferrer'],
+        },
+      ],
       [
         rehypeComponents,
         {
